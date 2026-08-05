86822fa0eb30ab52beedc8129c24ddb4e37b7d2a
Irma-Rodriguez <irodriguezcardenas@byupathway.edu>
2026-08-04 20:53:04 -0500

    Adding index and server details

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+const dns = require("node:dns/promises");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3003;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => { console.log(`Running on port ${port}`) });
+
+
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => { console.log(`Database is listening and node running on port ${port}`) });
+    }
+});
+
